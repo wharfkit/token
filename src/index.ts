485f62a98cd6c27d28b51aff55ae5e32914d91f4
dafuga <danielfugere28@gmail.com>
2023-09-07 19:40:19 -0700

    chore: added transfer method

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,45 @@
 import {APIClient, Asset, Name, NameType} from '@wharfkit/antelope'
 import * as SystemTokenContract from './contracts/system.token'
 
-interface BalanceOptions {
+interface TokenOptions {
     apiClient: APIClient
+    systemTokenSymbol?: Asset.SymbolType
 }
 
 export class Token {
     readonly systemTokenContract: SystemTokenContract.Contract
+    readonly systemTokenSymbol: Asset.SymbolType
 
-    constructor({apiClient}: BalanceOptions) {
+    constructor({apiClient, systemTokenSymbol}: TokenOptions) {
         this.systemTokenContract = new SystemTokenContract.Contract({client: apiClient})
+        this.systemTokenSymbol = systemTokenSymbol || '4,EOS'
+    }
+
+    async transfer(
+        from: NameType,
+        to: NameType,
+        amount: number,
+        memo = '',
+        symbolType: Asset.SymbolType = this.systemTokenSymbol
+    ) {
+        const symbol = Asset.Symbol.from(symbolType)
+        const senderBalance = await this.balance(from, symbol.code)
+
+        // check recipient balance to make sure that it exists
+        await this.balance(to, symbol.code)
+
+        const quantity = Asset.from(amount, symbol)
+
+        if (senderBalance.value < amount) {
+            throw new Error(`Insufficient funds for ${from} to transfer ${quantity} to ${to}.`)
+        }
+
+        return this.systemTokenContract.action('transfer', {
+            from: Name.from(from),
+            to: Name.from(to),
+            quantity,
+            memo,
+        })
     }
 
     balance(accountName: NameType, symbolCode?: Asset.SymbolCodeType): Promise<Asset> {
@@ -17,12 +47,16 @@ export class Token {
             .table('accounts', accountName)
             .all()
             .then((accountBalances) => {
+                if (!accountBalances) {
+                    throw new Error(`Account ${accountName} does not exist.`)
+                }
+
                 let accountBalance
 
                 if (symbolCode) {
-                    accountBalance = accountBalances.find((account) =>
-                        account.balance.symbol.code.equals(symbolCode)
-                    )
+                    accountBalance = accountBalances.find((account) => {
+                        return account.balance.symbol.code.equals(symbolCode)
+                    })
 
                     if (!accountBalance) {
                         throw new Error(
@@ -30,7 +64,7 @@ export class Token {
                         )
                     }
                 } else {
-                    accountBalance = accountBalances?.[0]
+                    accountBalance = accountBalances[0]
 
                     if (!accountBalance) {
                         throw new Error(`No balances found for ${accountName}.`)
--- test/tests/index.ts
+++ test/tests/index.ts
@@ -1,46 +1,78 @@
 import {strict as assert} from 'assert'
 
-import {Asset} from '@wharfkit/antelope'
+import {Asset, Serializer} from '@wharfkit/antelope'
 import {makeClient} from '@wharfkit/mock-data'
+
+import * as SystemTokenContract from '../../src/contracts/system.token'
 
 const apiClient = makeClient('https://jungle4.greymass.com')
 
 import {Token} from '../../src'
 
 suite('Token', function () {
+    const token = new Token({
+        apiClient,
+    })
+
     this.slow(20000)
 
     suite('balance()', function () {
         test('returns the system token balance of an account when no symbol code is passed', async function () {
-            const balance = new Token({
-                apiClient,
-            })
-            const result = await balance.balance('teamgreymass')
+            const result = await token.balance('teamgreymass')
             assert.equal(result instanceof Asset, true)
             assert.equal(String(result.symbol.code), 'EOS')
         })
 
         test('returns the balance of an account for a specific symbol', async function () {
-            const balance = new Token({
-                apiClient,
-            })
-            const result = await balance.balance('teamgreymass', 'EOS')
+            const result = await token.balance('teamgreymass', 'EOS')
             assert.equal(result instanceof Asset, true)
             assert.equal(String(result.symbol.code), 'EOS')
         })
 
         test('throws an error when the account does not exist', async function () {
-            const balance = new Token({
-                apiClient,
-            })
-            await assert.rejects(() => balance.balance('notanaccount'))
+            await assert.rejects(
+                () => token.balance('notanaccount'),
+                "Account 'notanaccount' does not exist."
+            )
         })
 
         test('throws an error when the symbol does not exist', async function () {
-            const balance = new Token({
-                apiClient,
+            await assert.rejects(
+                () => token.balance('teamgreymass', 'NOT'),
+                "Symbol 'NOT' does not exist."
+            )
+        })
+    })
+
+    suite('transfer()', function () {
+        test('creates a transfer action', async function () {
+            const action = await token.transfer(
+                'teamgreymass',
+                'teamgreymass',
+                1.32,
+                'this is a test'
+            )
+
+            assert.equal(String(action.account), 'eosio.token')
+            assert.equal(String(action.name), 'transfer')
+
+            const decoded = Serializer.decode({
+                data: action.data,
+                type: SystemTokenContract.Types.Transfer,
             })
-            await assert.rejects(() => balance.balance('teamgreymass', 'NOT'))
+            assert.equal(String(decoded.from), 'teamgreymass')
+            assert.equal(String(decoded.to), 'teamgreymass')
+            assert.equal(String(decoded.quantity), '1.3200 EOS')
+            assert.equal(String(decoded.memo), 'this is a test')
+        })
+
+        test('throws an error when one of the accounts does not exist', async function () {
+            await assert.rejects(() => token.transfer('teamgreymass', 'notanaccount', 1.32))
+            await assert.rejects(() => token.transfer('notanaccount', 'teamgreymass', 1.32))
+        })
+
+        test('throws an error when the balance of the sender is too low', async function () {
+            await assert.rejects(() => token.transfer('teamgreymass', 'dafuga.gm', 10000000))
         })
     })
 })
